d4ca0e41a3010cb9e8b33c2cb858720fc1f209c3
Wcosine <132649565+Wcosine@users.noreply.github.com>
2025-05-14 22:59:30 +0800

    Create CounterParents.vue

---
+++ Emits File/CounterParents.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { ref } from 'vue'
+  import CounterChild from './CounterChild.vue'
+
+  const counterTimes = ref(0)
+  // 除了設定一個空間儲存子組件帶來的資料, 也為了初始內容為 0
+  // 畫面顯示 : Count : 0 times
+  
+  const plus = (value) =>{
+    // value 為 子組件輸入的值
+    counterTimes.value = value++
+  }
+  const minus = (value) =>{
+    counterTimes.value = value--
+  }
+  const zero = (value) =>{
+    counterTimes.value = value
+  }
+  
+</script>
+
+<template>
+  <CounterChild @increace="plus" @decreace="minus" @zero="zero"/>
+  <p>Count : {{ counterTimes }} times</p>
+  <!-- 會自動解套, 所以不用加".value" -->
+</template>
