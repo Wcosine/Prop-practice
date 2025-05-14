ff3c75c461245e73bfaafdf50ee81cb7b4f559b0
Wcosine <132649565+Wcosine@users.noreply.github.com>
2025-05-14 16:25:37 +0800

    Create emitParents.vue

---
+++ Emits File/emitParents.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { ref } from 'vue'
+  import emitChild from './emitChild.vue'
+  
+  const message = ref('')
+  //設定一個儲存位置放子組件帶來的資料
+
+  const forMom = (msg) =>{
+    // 命名子組件內容為 msg 帶入父組件
+    message.value = msg
+  }
+  const forDear = (msg) =>{
+    // 命名子組件內容為 msg 帶入 父組件
+    message.value = msg
+  }
+  //接收並處理子組件來的資料
+  
+</script>
+
+<template>
+<emitChild @Mothers="forMom" @Valentines="forDear"/>
+<!-- 這裡要監聽子組件的動作,所以要用子組件定義的名稱 -->
+<h1> {{ message }} </h1>
+</template>
